55c016f2fc2659086e6b3d513d8e9c4982221eb0
Sam <contactsamie@gmail.com>
2014-07-13 00:29:52 -0400

    Updated folder name

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,10 @@
  */
 angular.module('nglarApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
   });
+
